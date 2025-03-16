972bd9cdc03d41da43c24cbb322415e6ef58d367
AliJohnnaton <trofimov2002@mail.ru>
2025-03-16 21:26:47 +0300

    Book2

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private final String name;
     private final String surname;
@@ -17,5 +19,23 @@ public class Author {
 
     public String getFullName() {
         return this.name + " " + this.surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Имя='" + name + '\'' +
+                ", фамилия='" + surname + '\'';
     }
 }
